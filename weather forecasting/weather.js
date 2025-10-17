00f7550dd27347c47bd005fdee1e3aa75cc0bad9
Krish-Sanghani-013 <25031701037@darshan.ac.in>
2025-10-17 15:00:37 +0530

    some changes

--- weather forecasting/weather.js
+++ weather forecasting/weather.js
@@ -23,15 +23,15 @@ async function checkWeather(city) {
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
     if (data.weather[0].main == "Clouds") {
-      weatherIcon.src = "/assets/clouds.png";
+      weatherIcon.src = "./assets/clouds.png";
     } else if (data.weather[0].main == "Clear") {
-      weatherIcon.src = "/assets/clear.png";
+      weatherIcon.src = "./assets/clear.png";
     } else if (data.weather[0].main == "Rain") {
-      weatherIcon.src = "/assets/rain.png";
+      weatherIcon.src = "./assets/rain.png";
     } else if (data.weather[0].main == "Drizzle") {
-      weatherIcon.src = "/assets/drizzle.png";
+      weatherIcon.src = "./assets/drizzle.png";
     } else if (data.weather[0].main == "Mist") {
-      weatherIcon.src = "/assets/mist.png";
+      weatherIcon.src = "./assets/mist.png";
     }
 
     document.querySelector(".weather").style.display = "block";
